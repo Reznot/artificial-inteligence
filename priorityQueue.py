dec2a2fc7c198d1d4ca7e78e523e7549aeee8070
Reznot <Reznot24@gmail.com>
2019-01-11 21:03:43 +0100

    created priority queue

---
+++ priorityQueue.py
@@ -0,0 +1,18 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def isEmpty(self):
+        return bool(len(self.elements == 0))
+
+    def push(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def pop(self):
+        if self.isEmpty():
+            print("Queue is empty")
+            return None
+        else:
+            return heapq.heappop(self.elements)
