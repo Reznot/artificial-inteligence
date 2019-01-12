1fd8a42d5d5d54a92562ecb44994b5a4128dd3d7
Reznot <Reznot24@gmail.com>
2019-01-12 15:04:18 +0100

    Astar class converted into function

--- main.py
+++ main.py
@@ -12,24 +12,26 @@ target = _grid.table_nodes[19][19]
 
 start.set_start_node(target)
 
-class Astar:
-    def __init__(self, start_node, target_node, grid):
-        self.start_node = start_node
-        self.target_node = target_node
-        self.A_grid = grid
-        self.openSet = priorityQueue.PriorityQueue()
-        self.closedSet = []
-        self.current_node = None
-        self.checked = False
 
-        self.openSet.push(start_node, start_node.f_cost)
+def Astar(start_node, target_node, given_grid):
+    start_node = start_node
+    target_node = target_node
+    A_grid = given_grid
+    open_set = priorityQueue.PriorityQueue()
+    closed_set = []
+    current_node = None
+    checked = False
 
+    open_set.push(start_node, start_node.f_cost)
 
-        while not self.openSet.isEmpty() and not self.current_node == target_node:
-            self.current_node = self.openSet.pop()
-            self.closedSet.append(self.current_node)
+    while not open_set.isEmpty() and not current_node == target_node:
+        current_node = open_set.pop()
+        closed_set.append(current_node)
 
-            neighbors = self.current_node.get_neighbors(self.A_grid)
-            print(neighbors['e'].x)
+        neighbors = current_node.get_neighbors(A_grid)
+        print(neighbors['e'].x)
+
+            #TODO check_if_better
+
 
 Astar(start, target, _grid)
